af9aa6a11a3fbbcdcebc7a82344d776705102943
Lawrence McDaniel <lpm0073@gmail.com>
2018-12-01 12:26:35 -0600

    page layout experiments

--- django/app/settings.py
+++ django/app/settings.py
@@ -29,7 +29,7 @@ env = environ.Env(
 # False if not in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
-DEBUG = False
+#DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
-USE_I18N = True
+USE_I18N = False
 USE_L10N = True
 USE_TZ = True
 
